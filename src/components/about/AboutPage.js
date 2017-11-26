2c1c0d07c6dfbd155e75df21fe2ab6e8495bcd71
Damith <damithasanka@gmail.com>
2017-11-26 16:21:22 +0530

    add initial source

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class AboutPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+              About us                
+            </div>
+        );
+    }
+}
+export default AboutPage;
